531cd992455be36f493c6c8d43ff4e3834080487
Raphael Alves <rapha555lima@gmail.com>
2024-11-29 15:14:54 -0300

    comecei a criar umas funções para requisição no client.py, dps continuo

--- app/client.py
+++ app/client.py
@@ -4,3 +4,16 @@ from flask import render_template, request
 @app.route("/")
 def home():
     return render_template('index.html', par1="Boa tarde", par2="Cliente")
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/signin")
+def signin():
+    return render_template('cadastro.html')
+
+@app.route("/autentica", methods=['POST'])
+def autentica():
+    user = request.form['user']
+
