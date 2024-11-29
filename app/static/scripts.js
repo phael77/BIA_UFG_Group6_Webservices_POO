74f1cc23e3e961fbaeb1bded76806d5884cc38a2
Andre2-Bardo <andre.andre2@discente.ufg.br>
2024-11-29 20:28:14 -0300

    Fixing the snapping of the title when the toggle button is triggered

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,6 +1,7 @@
 // Get references to the sidebar and toggle button
 const sidebar = document.querySelector('.sidebar');
 const toggleButton = document.querySelector('.toggle-sidebar');
+const siteTitle = document.querySelector('.site-title')
 
 // Add event listener to the toggle button
 toggleButton.addEventListener('click', () => {
@@ -9,4 +10,6 @@ toggleButton.addEventListener('click', () => {
   
   // Toggle the button's movement class
   toggleButton.classList.toggle('move');
+
+  siteTitle.classList.toggle('move-when-menu-hidden')
 });
