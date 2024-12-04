7cb63da9ad630cc4bd4535e61c0cb3d250b98555
Andre2-Bardo <andre.andre2@discente.ufg.br>
2024-12-04 00:02:26 -0300

    Adicionando comentários em app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 import os
 
+# Criação do objeto Flask, que representa a aplicação web
 app = Flask(__name__)
-app.secret_key = '1234567890'  # Ensure to set a secret key for session management
 
-# Import routes from the routes file
-from app.client import *
+# Definindo uma chave secreta para gerenciar sessões (importante para funcionalidades como login)
+# A chave secreta é usada para proteger dados de sessão, como cookies.
+app.secret_key = '1234567890'  # Certifique-se de definir uma chave secreta segura para produção
 
+# Importa as rotas da aplicação, que são definidas em outro arquivo (provavelmente 'routes.py' ou algo similar)
+from app.client import *  # Supondo que o arquivo 'client.py' contém as rotas para o cliente
+
+# Condição para rodar a aplicação em modo de desenvolvimento quando o script for executado diretamente
 if __name__ == "__main__":
+    # O método app.run(debug=True) inicia o servidor de desenvolvimento do Flask com a opção de debug ativada
     app.run(debug=True)
